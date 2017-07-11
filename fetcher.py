dbe5ef5b617bfc7817b66bfa8b7fa570e385f6a6
gaboflowers <gabrielflores96.2@gmail.com>
2017-07-11 01:29:45 -0400

    Arreglado para 2017-2
    U-Campus cambió su diseño interno. Ahora fetcher puede extraer la información del nuevo catálogo. Se cambia "datos_ramo" por un diccionario en vez de lista.

--- fetcher.py
+++ fetcher.py
@@ -40,120 +40,139 @@ siglas = codigoDepto.keys()
 #retorna una lista (de cursos), con una lista de secciones en c/curso
 #el primer item de la lista es el codigo del departamento
 def parseCursosSopa(sopa):
-    cod_cursos = [tag['id'] for tag in sopa.findAll('h2',{'id':True})]
+	
+    #cod_cursos = [tag['id'] for tag in sopa.findAll('h2',{'id':True})]
+	h2_with_id = [tag for tag in sopa.findAll('h2') if 'id' in tag.attrs]
+	cod_cursos = [tag['id'] for tag in h2_with_id if tag['id'] != 'titulo']
 
-    str_navbar = sopa.find('ul',{'id':'navbar'})
+	str_navbar = sopa.find('ul',{'id':'navbar'})
     
-    cod_y_nombre_este_depto = str_navbar.text.split('\n')[2]
-    guion = cod_y_nombre_este_depto.find(' -')
-    codigo_este_depto = cod_y_nombre_este_depto[:guion]
+	cod_y_nombre_este_depto = str_navbar.text.split('\n')[2]
+	guion = cod_y_nombre_este_depto.find(' -')
+	codigo_este_depto = cod_y_nombre_este_depto[:guion]
+
+	if codigo_este_depto == "EI":
+		tag = sopa.find('ul',{'id':'navbar'})
+		nombre_depto = unidecode.unidecode(tag.findAll('li')[1].text[5:]).lower()
+		if nombre_depto[:3] == "esc": codigo_este_depto = "escuela"
+		elif nombre_depto[:11] == "area de ing": codigo_este_depto = "ing"
+		else: codigo_este_depto = "idiomas"   
+	departamento = [codigo_este_depto] # [codigo_este_depto, curso1, curso2, curso3, ...]
     
-    if codigo_este_depto == "EI":
-        tag = sopa.find('ul',{'id':'navbar'})
-        nombre_depto = unidecode.unidecode(tag.findAll('li')[1].text[5:]).lower()
-        if nombre_depto[:3] == "esc": codigo_este_depto = "escuela"
-        elif nombre_depto[:11] == "area de ing": codigo_este_depto = "ing"
-        else: codigo_este_depto = "idiomas"   
-    departamento = [codigo_este_depto] # [codigo_este_depto, curso1, curso2, curso3, ...]
-    
-    i = 0
-    tablas = sopa.findAll('table')
-    for tabla in tablas:
-        codigo_este_curso = cod_cursos[i]
-        nombre_y_codigo = sopa.find('h2',{'id':codigo_este_curso}).text.replace('\n','').replace('\t','') # UNICODE
-        item_curso = structHorario.Curso([nombre_y_codigo]) # [nombre+codigo curso, [datos], secc1, secc2, secc3, ...]
+	i = 0
+	tablas = sopa.findAll('table')
+	for tabla in tablas:
+		codigo_este_curso = cod_cursos[i]
+		tag_nombre_y_codigo = sopa.find('h2',{'id':codigo_este_curso})
+		try: #quito el <a href... de Programa del Curso, lo guardo aparte
+			tag_programa = tag_nombre_y_codigo.a.extract()
+			link_programa = tag_programa['href']
+		except AttributeError:
+			link_programa = None
+		nombre_y_codigo = tag_nombre_y_codigo.text.replace('\n','').replace('\t','') # UNICODE
+		item_curso = structHorario.Curso([nombre_y_codigo]) # [nombre+codigo curso, [datos], secc1, secc2, secc3, ...]
 
-        tag = sopa.find('h2',{'id':codigo_este_curso})
-        tag = tag.find_next('dl')
-        datos_raw = tag.findAll('dd') #UD, Requisitos, Equivalencias...
+		tag = tag_nombre_y_codigo
+		tags_datos_raw = tag.find_next('dl')
+		tags_keys = tags_datos_raw.findAll('dt') #UD, Requisitos, Equivalencias...
+		'''
+		datos_raw = tag.findAll('dd') #UD, Requisitos, Equivalencias...
+		datos = [dato.text.replace('\n','').replace('\t','').replace('\r','') for dato in datos_raw]
+		item_curso.append(datos)
+		'''
+		datos = dict()
+		for t in tags_keys:
+			try:
+				key = t.text
+				value = t.findNextSibling('dd').text
+				datos[key] = value
+			except AttributeError:	
+				print "attr error",codigo_este_curso
+				break
 
-        datos = [dato.text.replace('\n','').replace('\t','').replace('\r','') for dato in datos_raw]
-        '''datos = []
-        for dato in datos_raw:
-            texto_dato = dato.text.replace('\n','').replace('\t','').replace('\r','')
-            #if texto_dato >'''
-        item_curso.append(datos)
-        
-        secciones = tabla.tbody.findAll('tr')
-        for seccion in secciones:
-            #item_seccion = structHorario.Seccion() # [[Profes],Cupo,Ocupados,[Horario]]    
-            
-            datos_seccion = seccion.findAll('td')
+		item_curso.append(datos)
 
-            profes_seccion = datos_seccion[1].findAll('h1')
-            vector_profes = []
-            for profe in profes_seccion:
-                vector_profes.append(profe.text.replace('\n','').strip())
-            item_seccion = structHorario.Seccion([vector_profes]) # [[Profes],Cupo,Ocupados,[Horario]]    
-            
-            for j in range(2,4):
-                item_seccion.append(datos_seccion[j].text.replace('\n','').strip())
+		secciones = tabla.tbody.findAll('tr')
+		for seccion in secciones:
+			#item_seccion = structHorario.Seccion() # [[Profes],Cupo,Ocupados,[Horario]]    
 
-            horario_raw = datos_seccion[4]
-            horario_raw = repr(horario_raw).replace('\\t','').replace('\\n','').replace('\\r','')[4:-5].lower()
-            if horario_raw == "":
-                item_curso.append([])
-                continue
-            horario_raw = horario_raw.replace('\\xe1','a').replace('\\xe9','e').replace('\\xed','i')\
-                          .replace('\\xf3','o').replace('\\xfa','u').replace('\\xf1','nh').replace('\\xc1','A')\
-                          .replace('\\xc9','E').replace('\\xcd','I').replace('\\xbf','O').replace('\\xda','U')\
-                          .replace('\\xfc','u').replace('\\xdc','U')
-            
-            lista_horario = [] #lista de Bloques
-            for item in horario_raw.split('<br/>'):
-                colon = item.find(':')
-                tipo = item[:colon]
-                #print "tipo :",tipo <- debugging
-                if tipo == "catedra":
-                    tipo = "cat"
-                elif tipo == "auxiliar":
-                    tipo = "aux"
-                elif tipo == "laboratorio":
-                    tipo = "lab"
-                elif tipo == "control":
-                    tipo = "con"
-                elif tipo == "semana": #<-- Problemas diferenciando semanas de horas; no voy a parsear semanas (*)
-                    continue
-                else: tipo = "otro"
-                item = item.split(" ")
+			datos_seccion = seccion.findAll('td')
 
-                #print 'item: ',item # <- debugging
-                #print 'len item:', len(item), 'item:',item,'type item:',type(item) 
+			tags_profes_seccion = datos_seccion[1].findAll('h1')
+			vector_profes = []
+			for tag_profe in tags_profes_seccion:
+				vector_profes.append( tag_profe.text.strip() )
+			item_seccion = structHorario.Seccion([vector_profes]) # [[Profes],Cupo,Ocupados,[Horario]]    
 
-                largo_item = len(item)
-                indices = range(1,largo_item,4) #es un for "a la C", necesito jugar con los indices mas abajo
-                k = 0
-                while k < len(indices):
-                    j = indices[k]
-                    if item[j][:3] == "sem": #<-- Problemas (vease (*))
-                        k = len(indices)
-                        continue
-                    dia = item[j][:2] #structHorario.Bloque maneja dias como 'lu', 'ma', ...
-                    t_i = item[j+1]
-                    t_f = item[j+3].replace(',','').replace(';','')
-                    bloque2 = None
-                    if j+4 < largo_item:
-                        if item[j+4][0].isdigit(): #a veces un mismo dia tiene 2 bloques
-                            t_i2 = item[j+4]
-                            t_f2 = item[j+6]
-                            bloque2 = structHorario.Bloque(tipo,dia,t_i2,t_f2)
+			for j in range(2,4):
+				item_seccion.append(datos_seccion[j].text.replace('\n','').strip())
 
-                    #print "dia: "+dia,"t_i: "+t_i,"t_f: "+t_f <- debugging
-                    bloque = structHorario.Bloque(tipo,dia,t_i,t_f)
-                    
-                    lista_horario.append(bloque)
-                    k+=1
-                    if bloque2 != None:
-                        lista_horario.append(bloque2)
-                        k+=2 #<-- juego de indices
-                    
-            item_seccion.append(lista_horario)
-            #item_seccion.append(horario_raw.text)
-            
-            item_curso.append(item_seccion)
-        departamento.append(item_curso)
-        i+=1
-    return departamento
+			horario_raw = datos_seccion[4]
+			#el siguiente es un truco feisimo para quitar tildes unicode...
+			horario_raw = repr(horario_raw).replace('\\t','').replace('\\n','').replace('\\r','')[4:-5].lower()
+			if horario_raw == "":
+				item_curso.append([])
+				continue
+			horario_raw = horario_raw.replace('\\xe1','a').replace('\\xe9','e').replace('\\xed','i')\
+						  .replace('\\xf3','o').replace('\\xfa','u').replace('\\xf1','nh').replace('\\xc1','A')\
+						  .replace('\\xc9','E').replace('\\xcd','I').replace('\\xbf','O').replace('\\xda','U')\
+						  .replace('\\xfc','u').replace('\\xdc','U')
+
+			lista_horario = [] #lista de Bloques
+			for item in horario_raw.split('<br/>'):
+				colon = item.find(':')
+				tipo = item[:colon]
+				#print "tipo :",tipo <- debugging
+				if tipo == "catedra":
+					tipo = "cat"
+				elif tipo == "auxiliar":
+					tipo = "aux"
+				elif tipo == "laboratorio":
+					tipo = "lab"
+				elif tipo == "control":
+					tipo = "con"
+				elif tipo == "semana": #<-- Problemas diferenciando semanas de horas; no voy a parsear semanas (*)
+					continue
+				else: tipo = "otro"
+				item = item.split(" ")
+
+				#print 'item: ',item # <- debugging
+				#print 'len item:', len(item), 'item:',item,'type item:',type(item) 
+
+				largo_item = len(item)
+				indices = range(1,largo_item,4) #es un for "a la C", necesito jugar con los indices mas abajo
+				k = 0
+				while k < len(indices):
+					j = indices[k]
+					if item[j][:3] == "sem": #<-- Problemas (vease (*))
+						k = len(indices)
+						continue
+					dia = item[j][:2] #structHorario.Bloque maneja dias como 'lu', 'ma', ...
+					t_i = item[j+1]
+					t_f = item[j+3].replace(',','').replace(';','')
+					bloque2 = None
+					if j+4 < largo_item:
+						if item[j+4][0].isdigit(): #a veces un mismo dia tiene 2 bloques
+							t_i2 = item[j+4]
+							t_f2 = item[j+6]
+							bloque2 = structHorario.Bloque(tipo,dia,t_i2,t_f2)
+
+					#print "dia: "+dia,"t_i: "+t_i,"t_f: "+t_f <- debugging
+					bloque = structHorario.Bloque(tipo,dia,t_i,t_f)
+	
+					lista_horario.append(bloque)
+					k+=1
+					if bloque2 != None:
+						lista_horario.append(bloque2)
+						k+=2 #<-- juego de indices
+	
+			item_seccion.append(lista_horario)
+			#item_seccion.append(horario_raw.text)
+
+			item_curso.append(item_seccion)
+		departamento.append(item_curso)
+		i+=1
+	return departamento
 
 #retorna la sopa del codigo HTML del depto/sem/yr indicado
 def getDeptoSopa(depto,yr,sem):
@@ -168,7 +187,8 @@ def getDeptoSopa(depto,yr,sem):
     html = response.read()
     sopa = BS(html,'html.parser')
     return sopa
-        
+
+getSopaFast = lambda url: BS(urllib2.urlopen(urllib2.Request(url)).read(),'html.parser')
 #retorna una lista con las sopas del codigo HTML de cada depto
 def getCatalogoHTML(yr,sem):
     yr,sem = str(yr),str(sem)
@@ -237,8 +257,12 @@ def guardarCatalogo(catalogo,yr,sem,extension='fcfm',file_name=None):
             if len(datos_curso) < 2:
                 string = '!N' #!N: "no hay datos"
             else:
-                string = '!'+datos_curso[0]+';'+datos_curso[1]+";"
-                if len(datos_curso) == 3:  string += datos_curso[2]+";"
+                string = '!'+datos_curso['UD']+';'+datos_curso['Requisitos']+";"
+                if len(datos_curso) == 3:
+					try:
+						string += datos_curso['Equivalencias']+";"
+					except KeyError:
+						pass
             #print "string:",string
             save_file.write(unidecode.unidecode(string)+"\n")   #ej: !10;(MA1002/MA1A2),(MA1102/MA1B2);MA2A1/MA22A/MA220
                                             #porque CVV tiene 10 UD, y pide C.Diferencial y A.Lineal
@@ -317,8 +341,17 @@ def cargarCatalogo(nombre_archivo):
             curso.append(nombre_y_curso) # <- curso[0]
             curso.setNombre()
         elif primer_char == "!": # datos de curso
-            datos = item[1:-1].split(";")[:-1]
-            curso.append(datos)
+			datos = item[1:-1].split(";")[:-1]
+			if datos[0] != "N":
+				dict_datos = {'UD':datos[0]}
+				try:
+					dict_datos['Requisitos'] = datos[1]
+					dict_datos['Equivalencias'] = datos[2]
+				except IndexError:
+					pass
+			else:
+				dict_datos = {}
+			curso.append(dict_datos)
         elif primer_char != "\n": #item es un string seccion
             datos_seccion = item.replace('\n','').split(";")
                                 
